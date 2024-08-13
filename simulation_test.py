716da2a14f059ccdbec122d29a774fc0ebe0d4d7
rajvinder-kaur <kaurrajvinder736@gmail.com>
2024-08-13 23:18:34 +0530

    wrote code for parameter sweep with loop

--- simulation_test.py
+++
@@ -1,9 +0,0 @@
-import imp
-import os
-os.add_dll_directory("C:\\Program Files\\Lumerical\\v202\\api\\python")
-lumapi = imp.load_source("lumapi", "C:\\Program Files\\Lumerical\\v202\\api\\python\\lumapi.py")
-
-import lumapi
-
-import numpy as np 
-fdtd = lumapi.FDTD();
